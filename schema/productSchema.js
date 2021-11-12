8deb378d65707d47276477523ec3c1f3cb6c8f47
Aldo <aldo@MacBook-Pro-de-Aldo-2.local>
2021-11-11 22:55:59 -0600

    modificando schema

--- schema/productSchema.js
+++ schema/productSchema.js
@@ -1,21 +1,23 @@
 const Joi = require('joi');
 
 const id = Joi.string().uuid();
-const name = Joi.string().min(3).max(15);
-const price = Joi.number().integer().min(10);
-const image = Joi.string().uri();
+const name = Joi.string().min(3).max(30);
+const costo = Joi.number();
+const price = Joi.number();
+const iva = Joi.number();
 
 const createProductSchema = Joi.object({
   name: name.required(),
+  costo: costo.required(),
   price: price.required(),
-  image: image.required(),
-
+  iva: iva.required(),
 });
 
 const updateProductSchema = Joi.object({
   name: name,
+  costo: costo,
   price: price,
-  image: image
+  iva: iva
 });
 
 const getProductSchema = Joi.object({
